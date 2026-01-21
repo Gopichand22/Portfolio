5faa81ba6ce98219d6f8b31387513998cc25e9b1
Gopichand Reddy <62110262+Gopichand22@users.noreply.github.com>
2026-01-21 15:39:05 +0530

    Update TechstackClassic.js

--- src/components/About/TechstackClassic.js
+++ src/components/About/TechstackClassic.js
@@ -1,21 +1,4 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiHtml5,
-  DiCss3,
-  DiJavascript1,
-  DiJqueryLogo,
-  DiSass,
-  DiBootstrap,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
-import { TbBrandGolang } from "react-icons/tb";
-import { AiOutlineJava } from "react-icons/ai";
 
 function Techstack() {
   return (
